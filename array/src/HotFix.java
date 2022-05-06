554506aa7b40faf186879bbee69c442250e0d2b2
zhaozhoujie <z457555460@163.com>
2022-05-06 13:53:32 +0800

    push commit

--- array/src/HotFix.java
+++ array/src/HotFix.java
@@ -6,5 +6,6 @@ public class HotFix {
     public static void main(String[] args) {
         System.out.println("hot-fix test");
         System.out.println("master test");
+        System.out.println("push test");
     }
 }
